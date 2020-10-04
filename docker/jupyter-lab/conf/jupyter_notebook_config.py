abe053b5030abcc84cdb2fe530aa7515d6b2001c
Nate Whitehead <n.whitehead@elsevier.com>
2020-10-04 13:00:04 -0400

    fixing branch

--- docker/jupyter-lab/conf/jupyter_notebook_config.py
+++ docker/jupyter-lab/conf/jupyter_notebook_config.py
@@ -8,16 +8,11 @@ c.NotebookApp.open_browser = False
 c.NotebookApp.allow_root = True
 c.NotebookApp.notebook_dir = '/playground/notebooks'
 
-# TODO: Don't do this :)
-c.NotebookApp.password = ''
-c.NotebookApp.token = ''
-
-# Slightly better solution - need to set env
-# if 'PASSWORD' in os.environ:
-#     password = os.environ['PASSWORD']
-#     if password:
-#         c.NotebookApp.password = passwd(password)
-#     else:
-#         c.NotebookApp.password = ''
-#         c.NotebookApp.token = ''
-#     del os.environ['PASSWORD']
+if 'PASSWORD' in os.environ:
+    password = os.environ['PASSWORD']
+    if password:
+        c.NotebookApp.password = passwd(password)
+    else:
+        c.NotebookApp.password = ''
+        c.NotebookApp.token = ''
+    del os.environ['PASSWORD']
