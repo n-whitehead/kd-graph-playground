2f6488f5b2957803854845a22f764f9f0b97ad70
Nate Whitehead <n.whitehead@elsevier.com>
2020-11-19 10:32:05 -0500

    refactoring images, moved to conda (bigger image...)

---
+++ jupyter/conf/jupyter_notebook_config.py
@@ -0,0 +1,11 @@
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = 8888
+c.NotebookApp.open_browser = False
+c.NotebookApp.allow_root = True
+c.NotebookApp.notebook_dir = '/playground/notebooks'
+
+# Insecure, but only locally available.
+c.NotebookApp.password = ''
+c.NotebookApp.token = ''
+
+c.IPKernelApp.matplotlib = 'inline'
